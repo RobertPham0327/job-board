78024be7b9767ad4f91f062d12c117431cd5571f
JustAbusinesse <huysanti123456@gmail.com>
2024-06-12 15:22:15 +0700

    create database and seeder for employer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Career;
+use App\Models\Employer;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,12 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        Career::factory()->count(25)->create();
+        // Career::factory()->count(25)->create();
+        Employer::factory()->count(20)->create();
     }
 }
+
