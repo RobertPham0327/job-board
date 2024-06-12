140374616f11185783d74578610c2cd59c5a14d6
JustAbusinesse <huysanti123456@gmail.com>
2024-06-12 14:30:14 +0700

    Refactor Time: Plain Vanilla JavaScript to Alpine.js

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,17 +1,17 @@
 <x-layout>
     <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index')]" />
-    <x-card class="mb-4 text-sm">
-        <form action="{{ route('jobs.index') }}" method="GET">
+    <x-card class="mb-4 text-sm" x-data="">
+        <form x-ref="filters" id="filtering-form" action="{{ route('jobs.index') }}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" />
+                    <x-text-input name="search" value="{{ request('search') }}" placeholder="Search for any text" form-ref="filters" />
                 </div>
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
-                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
-                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
+                        <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" form-ref="filters" />
+                        <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" form-ref="filters" />
                     </div>
                 </div>
                 <div>
@@ -27,10 +27,10 @@
                     <x-radio-group name="category" :options="array_combine(
                         array_map('ucfirst', \App\Models\Career::$category),
                         \App\Models\Career::$category,
-                    )" />
+                    )" />   
                 </div>
             </div>
-            <button class="w-20 h-8 rounded-md bg-blue-400 hover:bg-slate-500 hover:text-white">Filter</button>
+            <x-button class="w-20 h-8 rounded-md bg-blue-400 hover:bg-slate-500 hover:text-white">Filter</x-button>
         </form>
         </div>
     </x-card>
