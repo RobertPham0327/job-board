b2b35a54cbe5de4cc6fa3b10fb077e36c0750aa2
JustAbusinesse <huysanti123456@gmail.com>
2024-06-12 10:27:18 +0700

    Breadcrumbs Navigation

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('', fn() => to_route('jobs.index'));     // By default will be jump to this route
 
-Route::resource('jobs', CareerController::class)->only(['index', 'show']);
+Route::resource('jobs', CareerController::class)->only(['index', 'show',]);
